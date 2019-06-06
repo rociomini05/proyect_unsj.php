5d16c78488fca0646266943b28d6d08760881c0d
Rocio Mini <39060639+rociomini05@users.noreply.github.com>
2019-06-06 19:00:47 -0300

    Create unsj_internas_secretarias.php

---
+++ unsj_internas_secretarias.php
@@ -0,0 +1,149 @@
+<style>
+    body {
+        font-family: "Droid Sans", sans-serif;
+        color: #a6acae;
+    }
+
+    #estilo_cuerpo_evento {
+        background-color: white !important;
+    }
+
+    /*HEAD */
+    #estilo_modificarevento {
+        background-color: #0084b4;
+        margin-bottom: 15px;
+    }
+
+    #estilo_titulomodificar {
+        color: white;
+        font-family: Calibri;
+        display: inline-block;
+    }
+
+
+
+    /* TABS */
+    .nav-tabs > li.active > a, .nav-tabs > li.active > a:hover, .nav-tabs > li.active > a:focus {
+        background-color: white !important;
+    }
+
+    #estilo_tabs {
+        font-family: Calibri;
+        font-size: 13px;
+        font-weight: bold;
+        color: #7c7c7c !important;
+    }
+
+    #estilo_tabs :focus {
+        background-color: #349bb7 !important;
+        color: white !important;
+    }
+
+    #estilo_tabs :hover {
+        background-color: #00aad8 !important;
+        color: white !important;
+    }
+
+    #estilo_tabs :visited {
+        background-color: red !important;
+        color: white !important;
+    }
+
+
+    
+
+
+    /*BOTON*/
+    #continuar {
+
+        margin-bottom: 5%;
+    }
+
+    td{
+        padding-top: 15px !important; 
+        padding-bottom: 15px !important;
+    }
+    th{
+         padding-top: 15px !important; 
+        padding-bottom: 15px !important;s
+
+    }
+
+
+</style>
+
+
+
+
+<div class="container">
+    <div class="col-md-2"></div>
+    <div class="col-md-8" id="estilo_cuerpo_evento">
+        <div class="row">
+            <div class="col-md-12" id="estilo_modificarevento">
+                <h3 id="estilo_titulomodificar">SECRETARÍAS</h3>
+                
+            </div>
+        </div>
+       
+        <!-- GENERAL -->
+        <table class="table table-hover">
+    <thead>
+   
+    </thead>
+    <tbody>
+      <tr>
+        <td>ACADÉMICA (SAC)</td>
+        <td><a href="">Ver eventos</a></td>
+              
+      </tr>
+      <tr>
+        <td>ADMINISTRATIVO FINANCIERA (SAF)</td>
+         <td><a href="">Ver eventos</a></td>
+
+    
+      </tr>
+      <tr>
+        <td>BIENESTAR UNIVERSITARIO (SECBIEN)</td>
+         <td><a href="">Ver eventos</a></td>
+            
+      </tr>
+
+      <tr>
+        <td>CIENCIA Y TÉCNICA (SECYT)</td>
+         <td><a href="">Ver eventos</a></td>
+            
+      </tr>
+      <tr>
+        <td>COMUNICACIÓN (SECCOM)</td>
+         <td><a href="">Ver eventos</a></td>
+            
+      </tr>
+      <tr>
+        <td>EXTENSIÓN UNIVERSITARIA (SEU))</td>
+         <td><a href="">Ver eventos</a></td>
+            
+      </tr>
+      <tr>
+        <td>OBRAS Y SERVICIOS (SOYS)</td>
+         <td><a href="">Ver eventos</a></td>
+            
+      </tr>
+      <tr>
+        <td>POSGRADO Y RELACIONES INTERNACIONALES (SPRRII)</td>
+         <td><a href="">Ver eventos</a></td>
+            
+      </tr>
+    </tbody>
+  </table>
+  <hr>
+
+       
+</div>
+
+
+           
+
+  
+    <div class="col-md-2"></div>
+</div>
+
